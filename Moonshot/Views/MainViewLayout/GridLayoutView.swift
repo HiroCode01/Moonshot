0b7bedc46cc97694778392d11b826d76a3780bbb
FazliddinAbdazimov <abdazimovfazliddin27@gmail.com>
2025-05-25 02:13:23 +0500

    sorting files

---
+++ Moonshot/Views/MainViewLayout/GridLayoutView.swift
@@ -0,0 +1,61 @@
+//
+//  GridLayoutView.swift
+//  Moonshot
+//
+//  Created by Fazliddin Abdazimov on 25/05/25.
+//
+
+import SwiftUI
+
+struct GridLayoutView: View {
+    var missions: [Mission]
+    var astronauts: [String: Astronaut]
+
+    let columns = [GridItem(.adaptive(minimum: 150))]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(missions) {mission in
+                    NavigationLink {
+                        MissionView(mission: mission, astronauts: astronauts)
+                    } label: {
+                        VStack {
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .padding()
+                            
+                            VStack {
+                                Text("\(mission.displayName)")
+                                    .font(.headline)
+                                    .foregroundStyle(.white)
+                                
+                                Text(mission.formattedLaunchDate)
+                                    .font(.caption)
+                                    .foregroundStyle(.white.opacity(0.7))
+                            }
+                            .padding(.vertical)
+                            .frame(maxWidth: .infinity)
+                            .background(.lightBackground)
+                        }
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.lightBackground)
+                        )
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+        }
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    GridLayoutView(missions: missions, astronauts: astronauts)
+        .preferredColorScheme(.dark)
+}
